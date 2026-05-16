9b130ac48d60386088bb09e0e126045d3375c2a8
Chetan <kaushalchetan669@gmail.com>
2026-05-16 23:08:38 +0200

    comment

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,5 +1,4 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('electronAPI', {
   saveBackup: (data) => ipcRenderer.invoke('save-backup', data),
   loadBackup: () => ipcRenderer.invoke('load-backup'),
